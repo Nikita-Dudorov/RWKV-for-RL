c57f099ad1aa991500d56f25412b928b8c037866
Nikita-Dudorov <zoom_ccss@mail.ru>
2024-03-03 15:36:45 +0300

    ...

--- config.py
+++ config.py
@@ -22,25 +22,27 @@ class TmazeArgs:
 
         # train
         self.n_env_steps = 1e5
+        self.n_envs = 8
         self.rollout_len = 256
-        self.batch_size = 64
+        self.minibatch = 64
         self.n_epochs = 10
         self.lr = 1e-3
         self.lr_decay = True
         self.max_grad_norm = 0.5
 
         # log
-        self.log_every = 10
+        self.log_every = 10000
         self.save_every = None
         self.save_path = os.path.join('checkpoints', self.env_id)
 
         # eval
-        self.eval_every = 100
+        self.eval_every = 100000
         self.n_eval_episodes = 100
         self.videos = False
 
         # agent
-        self.n_hidden = 64
+        self.d_model = 64
+        self.d_ac = 64
         self.ppo_eps = 0.2
         self.ppo_eps_decay = True
         self.gamma = 0.98
