600d67ed1f47d38ac2573a6de2c77d2e427560c8
Nikita-Dudorov <zoom_ccss@mail.ru>
2024-03-03 15:36:08 +0300

    rwkv agent

--- agent.py
+++ agent.py
@@ -1,89 +1,45 @@
 import torch
-from torch.distributions.categorical import Categorical
-from torch.distributions.normal import Normal
-from torch import nn
+from actor_critic import DiscreteActorCritic
+from rwkv import RwkvCell
 
-class ContinuousActorCritic(nn.Module):
-    """Implements actor-critic agent for row observation and continuous action space"""
+class RwkvAgent():
 
-    def __init__(self, n_hidden, obs_dim, act_dim):
-        super().__init__()
-
-        self._critic = nn.Sequential(
-            nn.Linear(obs_dim, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, 1, bias=True)
+    def __init__(
+            self,
+            d_model,
+            d_ac,
+            obs_dim,
+            act_dim,
+        ):
+        self.encoder = torch.nn.Linear(obs_dim, d_model)
+        self.seq_model = RwkvCell(d_model)
+        self.ac = DiscreteActorCritic(
+            n_hidden=d_ac, 
+            obs_dim=d_model,
+            act_dim=act_dim, 
         )
-        
-        self._actor_mean = nn.Sequential(
-            nn.Linear(obs_dim, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, act_dim, bias=True)
-        )
+        self.rec_state = None
 
-        self._actor_std = nn.Parameter(torch.zeros(1, act_dim))
+    def reset_rec_state(self):
+        self.rec_state = self.seq_model.get_initial_state()
 
-        # TODO which layer init to use?
-        def init_weights(layer, std=1.0, bias=0.0):
-            if type(layer) == nn.Linear:
-                nn.init.orthogonal_(layer.weight, std)
-                # nn.init.xavier_normal_(layer.weight)
-                nn.init.constant_(layer.bias, bias)
-        
-        self.apply(init_weights)
+    def get_hidden(self, obs):
+        x = self.encoder(obs)
+        x, self.rec_state = self.seq_model(x, self.rec_state)
+        return x
 
-    def get_value(self, obs):
-        return self._critic(obs)
+    def get_action_and_value(self, obs):
+        x = self.get_hidden(obs)
+        action = self.ac.get_action(x)
+        value = self.ac.get_value(x)
+        return action, value
 
     def get_action(self, obs):
-        act_mean = self._actor_mean(obs)
-        act_std = self._actor_std.expand_as(act_mean).exp()
-        dist = Normal(act_mean, act_std)
-        act = dist.sample()
-        # return action, action probability, entropy of action distribution
-        return act, dist.log_prob(act).sum(1).exp(), dist.entropy().sum(1)  # suppose independetnt components -> summation over action dim 
-
-class DiscreteActorCritic(nn.Module):
-    """Implements actor-critic agent for row observation and discrete action space"""
-
-    def __init__(self, n_hidden, obs_dim, act_dim):
-        super().__init__()
-
-        self._critic = nn.Sequential(
-            nn.Linear(obs_dim, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, 1, bias=True)
-        )
-        
-        self._actor = nn.Sequential(
-            nn.Linear(obs_dim, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, n_hidden, bias=True),
-            nn.Tanh(),
-            nn.Linear(n_hidden, act_dim, bias=True)
-        )
-
-        # TODO which layer init to use?
-        def init_weights(layer, std=1.0, bias=0.0):
-            if type(layer) == nn.Linear:
-                nn.init.orthogonal_(layer.weight, std)
-                # nn.init.xavier_normal_(layer.weight)
-                nn.init.constant_(layer.bias, bias)
-        
-        self.apply(init_weights)
+        x = self.get_hidden(obs)
+        action = self.ac.get_action(x)
+        return action
 
     def get_value(self, obs):
-        return self._critic(obs)
-
-    def get_action(self, obs):
-        logits = self._actor(obs)
-        dist = Categorical(logits=logits)
-        act = dist.sample()
-        # return action, action probability, entropy of action distribution
-        return act, dist.log_prob(act).exp(), dist.entropy()
+        x = self.get_hidden(obs)
+        value = self.ac.get_value(x)
+        return value
